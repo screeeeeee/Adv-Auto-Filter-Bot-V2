8fda3ae843e0d6c8265d0ba59b9e52fd45f18622
screeeeeee <102519114+screeeeeee@users.noreply.github.com>
2022-03-28 15:45:57 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/scrooge010"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/scrooge010'),
+        InlineKeyboardButton('We Are 🧾', url ='https://t.me/asiandramagroup1')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Join With Us 🛠', url='https://t.me/asiandramagroup1')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
